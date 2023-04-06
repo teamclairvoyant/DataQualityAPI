953a4f89b5affbf1f4f873f3bb19345fd0fc2701
Siddharth Jamadari <Siddharth198832@exlservice.com>
2023-04-06 13:20:13 +0530

    removed unwanted imports

--- src/main/java/com/cv/dataqualityapi/rest/ClientRest.java
+++ src/main/java/com/cv/dataqualityapi/rest/ClientRest.java
@@ -9,14 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cv.dataqualityapi.model.Clients;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
-@Api(value = "/Client")
 @RequestMapping("/Client")
 public interface ClientRest {
 
-	@ApiOperation(value = "Get all the Clients", notes = "No input params are required", response = Clients.class, responseContainer = "List")
 	@GetMapping(value = "getClients")
 	public List<Clients> getClients();
 
